bc026f5d9452d6c2a9573a2621a05c6285c227ac
opacitySun <809340810@qq.com>
2016-08-18 16:35:53 +0800

    更新修改

--- app.js
+++ app.js
@@ -46,27 +46,8 @@ app.use(express.query());
 app.use('/', wechat(config, function (req, res, next) {
   // 微信输入信息都在req.weixin上
   var message = req.weixin;
-  if (message.FromUserName === 'diaosi') {
-    // 回复屌丝(普通回复)
-    res.reply('hehe');
-  } else if (message.FromUserName === 'text') {
-    //你也可以这样回复text类型的信息
-    res.reply({
-      content: 'text object',
-      type: 'text'
-    });
-  } else if (message.FromUserName === 'hehe') {
-    // 回复一段音乐
-    res.reply({
-      type: "music",
-      content: {
-        title: "来段音乐吧",
-        description: "一无所有",
-        musicUrl: "http://mp3.com/xx.mp3",
-        hqMusicUrl: "http://mp3.com/xx.mp3",
-        thumbMediaId: "thisThumbMediaId"
-      }
-    });
+  if (message.MsgType === 'text') {
+    res.reply({ type: "text", content: "你说 " + message.Content});
   } else {
     // 回复高富帅(图文回复)
     res.reply([
